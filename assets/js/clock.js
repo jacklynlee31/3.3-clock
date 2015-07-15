e73f21cdf7dc147b4d146e37eb31782931878785
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-07-15 16:53:27 -0500

    Colors change, working on time.

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,1 +1,19 @@
-alert("Hello! I am an alert box!!");
+var timeEL = document.getElementById('time');
+
+var nextOpacity = 0;
+
+window.setInterval(function () {
+	var d = new Date();
+		d.getHours
+		d.getMinutes
+		d.getSeconds
+    var r = document.getElementById('radial-2');
+    r.style.opacity = nextOpacity;
+
+	if (nextOpacity == 1) {
+		nextOpacity = 0;
+	} else {
+		nextOpacity = 1;
+	}
+}, 1000);
+
