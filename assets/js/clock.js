cef0aea1bb4f0ddd10be5fec3de2c3c5ca9a63df
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-07-16 16:23:28 -0500

    Updating things.

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,12 +1,11 @@
-var timeEL = document.getElementById('time');
-
 var nextOpacity = 0;
 
 window.setInterval(function () {
-	var d = new Date();
-		d.getHours
-		d.getMinutes
-		d.getSeconds
+	var currentTime = new Date ();
+	var hours = currentTime.getHours ();
+	var minutes = currentTime.getMinutes ();
+	var seconds = currentTime.getSeconds ();
+
     var r = document.getElementById('radial-2');
     r.style.opacity = nextOpacity;
 
@@ -16,4 +15,32 @@ window.setInterval(function () {
 		nextOpacity = 1;
 	}
 }, 1000);
+
+clock.innerHTML = padZeros(hours) + ':' + padZeros(minutes) + ':' + padZeros(seconds);
+
+// var timeEL = document.getElementById('time');
+
+// document.getElementById ("clock").innerHTML = "hours:minutes:seconds";
+
+// var clockEl = document.getElementById("clock");
+
+
+// 	var timeString = "hours:minutes:seconds" ;
+// }
+// then later do `clock.innerHTML = someVariable`l
+//
+// var x = "hour:minute:second"
+// undefined
+// x
+// "hour:minute:second"
+
+//add a 0 before single digit numbers
+
+// function padZeros (num) {
+//     if (num < ___) {
+//         return '0' + ___;
+//     }
+
+//     return ___;
+// }
 
