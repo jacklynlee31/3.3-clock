f5ef7793e091736ff86806dbe5192d2a143466dd
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-07-22 09:23:00 -0500

    Attempt 2

--- clock.js
+++ clock.js
@@ -1,40 +1,20 @@
-var timeEL = document.getElementById('time');
-
+var timeEL = document.getElementById('clock');
 var nextOpacity = 0;
 
-window.setInterval(function () {
+function padZeros (num) {
+    if (num < 10) {
+        return '0' + num;
+    }
 
-document.getElementById ("clock").innerHTML = "hours:minutes:seconds";
-
-var clockEl = document.getElementById ("clock");
-
-function time(){
-	var currentTime = new Date ();
-	var hours = currentTime.getHours ();
-	var minutes = currentTime.getMinutes ();
-	var seconds = currentTime.getSeconds ();
-
-	var timeString = "hours:minutes:seconds" ;
-
-
-// then later do `clock.innerHTML = someVariable`l
-//
-// var x = "hour:minute:second"
-// undefined
-// x
-// "hour:minute:second"
-
-//add a 0 before single digit numbers
-
-// function padZeros (num) {
-//     if (num < ___) {
-//         return '0' + ___;
-//     }
-
-//     return ___;
-// }
-
+    return num;
 }
+
+window.setInterval(function () {
+	var currentTime = new Date();
+	var hours = currentTime.getHours();
+	var minutes = currentTime.getMinutes();
+	var seconds = currentTime.getSeconds();
+
     var r = document.getElementById('radial-2');
     r.style.opacity = nextOpacity;
 
@@ -43,5 +23,18 @@ function time(){
 	} else {
 		nextOpacity = 1;
 	}
+
+	timeEL.innerHTML = padZeros(hours) + ':' + padZeros(minutes) + ':' + padZeros(seconds);
 }, 1000);
+
+// 	var timeString = "hours:minutes:seconds" ;
+// }
+// then later do `clock.innerHTML = someVariable`l
+//
+// var x = "hour:minute:second"
+// undefined
+// x
+// "hour:minute:second"
+
+//add a 0 before single digit numbers
 
